d40fab937c16fedabe280c94270004bae2abd0ca
Tom Ko <yungsu2391@gmail.com>
2022-08-23 10:20:00 +0900

    webpack 설정이 필요했음..기본공부 하면서 webpack 삽입하기

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,7 @@
 export const getLogin = (req, res) => {
-  return res.render("login");
+  return res.render("login", {
+    title: "Login",
+  });
 };
 
 export const postLogin = (res, req) => {
--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import { application } from "express";
 import rootRouter from "./routers/rootRouter";
+import morgan from "morgan";
 
 const app = express();
 const port = 4000;
+const logger = morgan(":url");
 
+app.use(logger);
 const handleListening = () =>
   console.log(`✅ server listening from http://localhost:${port} 🚀`);
 app.listen(port, handleListening);
 app.set("view engine", "ejs");
+console.log(__dirname);
 app.set("views", process.cwd() + "/src/views");
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use("/", rootRouter);
