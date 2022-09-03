e912cac85ebbfce38f5c3ea37c3a37e487a37a20
Tom Ko <yungsu2391@gmail.com>
2022-09-03 18:55:53 +0900

    db연결하기

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -1,9 +1,10 @@
+// db연결 어떻게 하지
+
 export const summaryController = (req, res) => {
   res.render("summary", {
     title: "Summary",
   });
 };
-
 export const naverController = (req, res) => {
   res.render("naver", {
     title: "Naver",
