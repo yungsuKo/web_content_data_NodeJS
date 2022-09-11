2f0ddfa045eefc2fc74bb2bcf5bb5d53f24fddc6
Tom Ko <yungsu2391@gmail.com>
2022-09-11 12:06:57 +0900

    table display complete

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -1,17 +1,25 @@
 // db연결 어떻게 하지
+import "../db";
+import BoonData from "../models/KaKao_1boon";
+import PostData from "../models/Naver_post";
 
 export const summaryController = (req, res) => {
   res.render("summary", {
     title: "Summary",
   });
 };
-export const naverController = (req, res) => {
-  res.render("naver", {
-    title: "Naver",
-  });
-};
 export const kakaoController = (req, res) => {
   res.render("kakao", {
     title: "KaKao",
+  });
+};
+export const naverController = async (req, res) => {
+  const posts = await PostData.find({})
+    .sort({ uploadTime: -1, crawledTime: -1 })
+    .limit(10);
+  console.log(posts);
+  res.render("naver", {
+    title: "Naver",
+    posts,
   });
 };
