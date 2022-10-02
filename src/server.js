16e0c758e0f7e9492f6536292e88fc29e012543d
Tom Ko <yungsu2391@gmail.com>
2022-10-02 18:38:35 +0900

    port edit

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ import "./db";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 const logger = morgan(":url");
 app.use(express.static(__dirname + "/public"));
 
