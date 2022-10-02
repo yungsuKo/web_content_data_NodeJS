a82d9d29b7bd81cada0fa5270967cfedb3b8f3ef
Tom Ko <yungsu2391@gmail.com>
2022-10-02 17:43:15 +0900

    db error

--- src/db.js
+++ src/db.js
@@ -4,10 +4,10 @@ try {
   mongoose.connect(
     "mongodb+srv://tom:DGFQ6qIUTADEgWlf@cluster0.v4qfqfc.mongodb.net/?retryWrites=true&w=majority"
   );
-  const db = mongoose.connection;
-
-  const handleOpen = () => console.log("connected");
-  db.once("open", handleOpen);
 } catch (e) {
   console.log(e);
 }
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("connected");
+db.once("open", handleOpen);
