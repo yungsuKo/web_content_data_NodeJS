d22f6c350216b751a9de6bfdf3f16727b9d283a5
Tom Ko <yungsu2391@gmail.com>
2022-10-21 22:38:34 +0900

    reframing, signup

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -23,6 +23,8 @@ export const kakaoController = (req, res) => {
 };
 export const naverController = async (req, res) => {
   try {
+    // 배열을 2중 배열로 넘겨야 할 듯
+    // 각 항목에 대한 기간별 데이터를 넘겨야 하기 때문
     const posts = await PostData.find({})
       .sort({ uploadTime: -1, crawledTime: -1 })
       .limit(10);
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -31,23 +31,40 @@ export const postLogin = async (req, res) => {
   }
   req.session.loggedIn = true;
   req.session.user = user;
-  return res.redirect("/data/summary");
+  return res.redirect("/data/naver");
 };
 
 export const getSignup = (req, res) => {
   return res.render("signup", {
     title: "Signup",
     pageTitle: "회원가입",
+    pageDescription : "모든 기능이 무료이니 회원가입 후 사용해보세요",
+    errorMsg : ""
   });
 };
 export const postSignup = async (req, res) => {
-  const { id, password } = req.body;
-  const isExist = await User.exists({ id });
-  if (isExist) return res.redirect("/signup");
-  const user = await User.create({ id, password });
+    console.log(req.body);
+    const { id, password, password2 } = req.body;
+    const isExist = await User.exists({ id });
+    if (isExist) return res.status(400).render("signup",{
+        title: "Signup",
+        pageTitle: "회원가입",
+        pageDescription : "모든 기능이 무료이니 회원가입 후 사용해보세요",
+        errorMsg : "존재하는 ID 입니다."
+    });
+    if(password !== password2){
+        return res.status(400).render("signup",{
+            title: "Signup",
+            pageTitle: "회원가입",
+            pageDescription : "모든 기능이 무료이니 회원가입 후 사용해보세요",
+            errorMsg : "비밀번호가 일치하지 않습니다."
+        });
+    }
 
-  console.log(user);
-  req.session.loggedIn = true;
-  req.session.user = user;
-  return res.redirect("/");
+    const user = await User.create({ id, password });
+
+    console.log(user);
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
 };
