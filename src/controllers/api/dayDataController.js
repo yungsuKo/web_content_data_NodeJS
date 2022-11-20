31f6023d83ba3536e36d5e47179b7411bd3ef200
Tom Ko <yungsu2391@gmail.com>
2022-11-21 08:30:29 +0900

    event listener add

---
+++ src/controllers/api/dayDataController.js
@@ -0,0 +1,95 @@
+import "../../db";
+import AccountUrl from "../../models/AccountUrl";
+import CrawlPostData from "../../models/CrawlUrlPost";
+import PostDetail from "../../models/PostDetail";
+
+export const apiDetailController = async (req, res) => {
+    try {
+        // 배열을 2중 배열로 넘겨야 할 듯
+        // 각 항목에 대한 기간별 데이터를 넘겨야 하기 때문
+        // 이거 데이터 뿌려주기 위한 데이터 구조를 다시 한 번 보는게 좋을 듯..
+        const {id} = req.params;
+        console.log(id);
+        const accounturl = await AccountUrl.findById(id);
+        let postUrls = [];
+        postUrls = await CrawlPostData.find({url:accounturl.url}).sort({uploadTime:-1}).lean().limit(7).populate("postDetails");
+        let semiResults = postUrls.map(post => {
+            let dateDiff = post.postDetails.map(detail => {
+                // ceil은 천장 - 무조건 올림을 의미함 
+                return Math.ceil((detail.createTime.getTime() - post.uploadTime.getTime())/(1000 * 60 * 60 * 24));
+            });
+            let postViews = post.postDetails.map(detail => {
+                return detail.views
+            });
+            let postLikes = post.postDetails.map(detail => {
+                return detail.likes
+            });
+            let postComments = post.postDetails.map(detail => {
+                return detail.comments
+            });
+            return {
+                dateDiff,
+                title: post.title,
+                img : post.img,
+                url : post.postUrl,
+                uploadTime: post.uploadTime,
+                views: postViews,
+                likes : postLikes,
+                comments : postComments
+            }
+        });
+        console.log("semiResults : ",semiResults);
+        async function getResultObject(arrItem){
+            let resultObject = {
+                title: String,
+                img : String,
+                uploadTime: Date,
+                url : String,
+                dateDiff:[],
+                views:[],
+                likes:[],
+                comments:[]
+            };
+            const list = [1,2,3,4,5,6,7];
+            for(const i of list){
+                console.log(i);
+                let index = arrItem.dateDiff.lastIndexOf(i,0);
+                resultObject.title = arrItem.title;
+                resultObject.img = arrItem.img;
+                resultObject.uploadTime = arrItem.uploadTime;
+                resultObject.url = accounturl.platform=="naver"?"https://post.naver.com/"+arrItem.url:"https:"+arrItem.url;
+                if(index > -1){
+                    resultObject.dateDiff[i-1] = arrItem.dateDiff[index];
+                    resultObject.views[i-1] = arrItem.views[index];
+                    resultObject.likes[i-1] = arrItem.likes[index];
+                    resultObject.comments[i-1] = arrItem.comments[index];
+                }else{
+                    resultObject.dateDiff[i-1] = null;
+                    resultObject.views[i-1] =  null;
+                    resultObject.likes[i-1] = null;
+                    resultObject.comments[i-1] = null;
+                }
+            }
+            return resultObject;
+        };
+        const result = [];
+        for(let i = 0;i < semiResults.length;i++){
+            let semiResult = semiResults[i];
+            let data = await getResultObject(semiResult);
+            result.push(data);
+        }
+        // semiResults.map(async (semiResult) => {
+        //     console.log(semiResult);
+        //     return await getResultObject(semiResult);
+        // })
+        
+        console.log("result",result);
+        res.render("detail_data", {
+            title: "detail",
+            result
+        });
+        
+    } catch (e) {
+        console.log(e);
+    }
+};
--- src/routers/dataRouter.js
+++ src/routers/dataRouter.js
@@ -4,10 +4,12 @@ import {
   postDataListController,
   dataDetailController
 } from "../controllers/dataController";
+import { apiDetailController } from "../controllers/api/dayDataController";
 import { protectorMiddleware } from "../middlewares";
 const dataRouter = express.Router();
 
 dataRouter.route("/").all(protectorMiddleware).get(getDataListController).post(postDataListController);
 dataRouter.route("/detail/:id([0-9a-f]{24})").all(protectorMiddleware).get(dataDetailController);
+dataRouter.route("/api/detail/:id([0-9a-f]{24})").all(protectorMiddleware).get(apiDetailController);
 
 export default dataRouter;
